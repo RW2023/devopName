77cf6aa808d4315ce193559aef7ad360951e7924
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-04-10 03:42:19 -0400

    change color pallette

--- src/js/app.js
+++ src/js/app.js
@@ -7,5 +7,36 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// Declare an async function to fetch data
+async function fetchData() {
+  try {
+    const response = await fetch('https://discord.com/api/guilds/1061557042806009927/widget.json');
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+// Use the fetchData function to get the data and destructure it
+(async () => {
+  const data = await fetchData();
+
+  if (data) {
+    // Destructure the data as needed
+    const { id, name, instant_invite, presence_count } = data;
+
+    // Use the destructured data
+    console.log('Guild ID:', id);
+    console.log('Guild Name:', name);
+    console.log('Instant Invite:', instant_invite);
+    console.log('Presence Count:', presence_count);
+  }
+})();
 
 
